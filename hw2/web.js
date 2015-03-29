c6c6770f51567e0ba3f01b3687a37481a5519739
alantea <alan094@gmail.com>
2015-03-30 04:49:06 +0800

    hw2 foodmenu finish

--- hw2/web.js
+++ hw2/web.js
@@ -16,49 +16,50 @@ document.getElementById("xmlform").onsubmit = function()
 	{	// for IE6, IE5
 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	xmlhttp.open("GET", url, true);
+	xmlhttp.open("GET", url);
 	xmlhttp.send();
-	// check the XML is correct
-	/*
-	if( xmlhttp.readyState != 4)
-	{
-		alert("Can't send xml file");
-		return false;
-	}
-	if ( xmlhttp.status != 200)
-	{
-		alert("Error in XML file2.");
-		return false;
-	}
-	*/
-	xmlDoc = xmlhttp.responseXML;
+	
 	var newWindow = window.open("", "", "width=800, height=600");
 
-	// header
-	newWindow.document.write( "<!DOCTYPE html>\n<html><head><meta charset=\"utf-8\"></head><body>" );
-	
-	var data_food = xmlDoc.getElementsByTagName("food");
-	/*
-	for( i = 0 ; i < data_food.length ; i++ )
-	{
-		newWindow.document.write("<div class=\"foodname\">" + data_food[i].getElementsByTagName[0].childNodes[0].nodeValue + "</div>");
-	}
-	*/
 
 	/* start to write data */
 	xmlhttp.onreadystatechange = function()
 	{
-		/* Food menu */
 		if (xmlhttp.readyState==4 && xmlhttp.status==200)
 		{
-			newWindow.document.write( "Write data" );
+			// header
+			newWindow.document.write( "<!DOCTYPE html>\n<html><head><meta charset=\"utf-8\">" );
+			newWindow.document.write( "<link rel=\"stylesheet\" type=\"text/css\" href=\"popup.css\"></head><body><center>" );
+			
+			var xmlDoc = xmlhttp.responseXML;
+
+			/* Food menu */
+			newWindow.document.write( "<h1>Food Menu</h1>" );
+
+			var data_food = xmlDoc.getElementsByTagName("food");
+			for( i = 0 ; i < data_food.length ; i++ )
+			{
+				newWindow.document.write("<div class=\"foodname\">" + data_food[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + "</div>");
+				newWindow.document.write("<div class=\"foodprice\">" + data_food[i].getElementsByTagName("price")[0].childNodes[0].nodeValue + "</div>");
+				newWindow.document.write("<div class=\"foodcalories\">(" + data_food[i].getElementsByTagName("calories")[0].childNodes[0].nodeValue + ")</div>");
+				newWindow.document.write("<div class=\"fooddescription\">" + data_food[i].getElementsByTagName("description")[0].childNodes[0].nodeValue + "</div>");
+			}
+			if( data_food.length == 0 )
+			{
+				newWindow.document.write("<div>No data.</div>");
+			}
+
+			/* sport menu */
+			newWindow.document.write( "<h1>Sports</h1>" );
 		}
+		else if (xmlhttp.readyState==4 && xmlhttp.status!=200)
+		{
+			console.log("Error: "+xmlhttp.status)
+		}
+		
+		// footer
+		newWindow.document.write( "</center></body></html>" );
 	}
-
-	// footer
-	newWindow.document.write( "</body></html>" );
-	
-
 
 	// prever the page data lose
 	return false;
